d2c062e93d98b7b960ebf40ab3a178db29f9963d
Ozzeek <osama.m.alnagar@gmail.com>
2024-01-08 03:03:13 +0200

    feat: created test and capitalize function

---
+++ index.js
@@ -0,0 +1,7 @@
+function capitalize(string) {
+  string = string.trimStart();
+  let capitalized = string.replace(string.at(0), string.at(0).toUpperCase());
+  return capitalized;
+}
+
+export default capitalize;
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import capitalize from "./index.js";
+
+test("Capitalize", () => {
+  expect(capitalize("ahmed").toBe("Ahmed"));
+});
